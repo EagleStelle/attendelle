6c3ef5191a623cd1a6af1074a3cda3852e1ddebb
EagleStelle <valentinopasno1277@gmail.com>
2026-07-06 12:26:37 +0800

    feat: add login page with form and routing
    - Implemented routing to the login page as the default route.
    - Created login component with HTML template for user authentication.
    - Added form fields for username and password with show/hide password functionality.
    - Included a "Remember me" checkbox and a submit button.
    - Updated styles to reflect new design choices and removed old CSS variables.

---
+++ frontend/src/app/pages/login/login.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideEye, lucideEyeOff } from '@ng-icons/lucide';
+import { HlmButton } from '@spartan-ng/helm/button';
+import { HlmCardImports } from '@spartan-ng/helm/card';
+import { HlmCheckbox } from '@spartan-ng/helm/checkbox';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
+import { HlmInput } from '@spartan-ng/helm/input';
+import { HlmLabel } from '@spartan-ng/helm/label';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    FormsModule,
+    NgIcon,
+    HlmCardImports,
+    HlmFieldImports,
+    HlmInput,
+    HlmLabel,
+    HlmButton,
+    HlmCheckbox,
+  ],
+  viewProviders: [provideIcons({ lucideEye, lucideEyeOff })],
+  templateUrl: './login.html',
+})
+export class Login {
+  protected readonly username = signal('');
+  protected readonly password = signal('');
+  protected readonly showPassword = signal(false);
+  protected readonly rememberMe = signal(false);
+
+  protected onSubmit(): void {
+    // TODO: wire authentication
+  }
+}
